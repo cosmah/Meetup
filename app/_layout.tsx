77b0806675777df134256361a6d4f29ab8b55a27
cosmah <cosmahke4@gmail.com>
2024-11-22 13:24:03 +0300

    seting up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,45 +1,10 @@
-import { Slot, SplashScreen, Stack } from 'expo-router'
-import { useFonts } from 'expo-font'
-import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from "react-native";
+import { Slot } from "expo-router";
 
-// import GlobalProvider from '../context/GlobalProvider'
-
-// Managing splash screen, prevent splash screen from auto hiding before assets load
-SplashScreen.preventAutoHideAsync();
-
-// Load fonts
 const RootLayout = () => {
-    const [fontsLoaded, error] = useFonts({
-        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
-    });
-    
-    useEffect(() => {
-        if(error) throw error;
-
-        if(fontsLoaded) SplashScreen.hideAsync();
-    }, [fontsLoaded, error])
-
-    if(!fontsLoaded && !error) return null;
-
   return (
-    <GlobalProvider>
-      <Stack>
-        <Stack.Screen name='index' options={{headerShown: false}}/>
-        <Stack.Screen name='(auth)' options={{headerShown: false}}/>
-        <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
-        <Stack.Screen name='search/[query]' options={{headerShown: false}}/>
-        <Stack.Screen name='views/bookingDetails' options={{headerShown: false}}/>
-      </Stack>
-    </GlobalProvider>
-  ) 
-}
+    <Slot />
+  );
+};
 
-export default RootLayout
+export default RootLayout;
